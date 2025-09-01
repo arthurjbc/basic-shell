dfc874ec4c1533ed693dea359554ede34a1f1abb
arthurjbc <arthurjorge1023@gmail.com>
2025-09-01 11:38:51 -0300

    background process added and a little refactor

--- shell.c
+++ shell.c
@@ -1,49 +1,87 @@
+/*
+next:
+    pipe: pipe()
+    I/O: open()
+    signal working on child: signal()
+    background process: father doesnt wait for child when & appears
+
+*/
+
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <signal.h>
 
-int main() {
-    char comando[1000];
-    char *arg[1000];
+void handleChdSignal(int sig) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
 
+void clear(){
     // initialize the process with clear command
     pid_t pid2 = fork();
     if (pid2 == 0){
         execlp("clear", "clear", NULL);
         exit(1);
-        perror("deu ruim");
+        perror("clear failed");
     }
     if (pid2 > 0){
         int st;
         wait (&st);
     }
+}
 
+int main() {
+    char comando[1000];
+    
+    clear();
+    
     while (1) {
-
-        printf("\033[34m%s\033[0m$ ", getcwd(NULL, 0));
+        char *arg[1000] = {};
+        signal(SIGCHLD, handleChdSignal);
+        int bg=0;
+        char *cwd = getcwd(NULL, 0);
+        printf("\033[33m%s@\033[0m\033[34m%s\033[0m$ ", getlogin(), cwd);
+        free (cwd);
         int i=0;
 
         // tranforms the input into a array (necessary for execvp)
         if (fgets(comando, sizeof(comando), stdin)!=NULL){
-            char *token = strtok(comando, " ");
+            char *token = strtok(comando, " \n"); 
             while (token != NULL){
                 arg[i] = token;
                 i++;
-                token = strtok(NULL, " ");
+                token = strtok(NULL, " \n"); 
             }
             arg[i] = NULL;
         }
-
-        arg[i-1][strcspn(arg[i-1], "\n")] = 0; // take the newLine off
         
-        if (strcmp(comando, "exit") == 0){   //built-in command exit
+        if (i>1 && strcmp(arg[i-1], "&")==0){  // handle background process request
+            arg[i-1] = NULL; 
+            bg = 1;
+        }
+
+        if (arg[0]==NULL){
+            continue;
+        }
+
+        
+        if (strcmp(arg[0], "exit") == 0){   // built-in command exit
+            printf("exit\n");
             exit(0);
         }
 
-        if (strcmp(arg[0], "cd")==0){   //built-in command cd
-            chdir(arg[1]);
+        if (strcmp(arg[0], "cd")==0){   // built-in command cd
+            if (arg[1]==NULL){
+                chdir("/home");
+            }else{
+                if (chdir(arg[1])==-1){
+                    perror("cd failed");
+            }
+            }
+
             continue;
         }
 
@@ -55,8 +93,12 @@ int main() {
             exit(1);
         }
         else if (pid > 0) {
-            int status;
-            wait(&status);
+            if (bg){
+                printf("[%d]Job running on background\n", pid);
+            }else{
+                int status;
+                wait(&status);
+            }
         }
     }
     
